22d3598747ad1a814b23d1c9c342209dbbe229ff
Corentin Ripaud <corentin.ripaud@gmail.com>
2024-02-21 14:45:55 +0100

    rails yelp mvp

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_21_114740) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_21_134226) do
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.string "address"
@@ -20,4 +20,14 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_21_114740) do
     t.string "phone_number"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.integer "rating"
+    t.string "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "restaurant_id"
+    t.index ["restaurant_id"], name: "index_reviews_on_restaurant_id"
+  end
+
+  add_foreign_key "reviews", "restaurants"
 end
